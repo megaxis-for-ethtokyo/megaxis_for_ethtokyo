9f368702559db113659e57c10821444c5e09ddea
trigy <89495755@qq.com>
2023-04-15 23:50:39 +0800

    front main api and ui

---
+++ megaxisFront/src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css'
+import AppRoutes from "./routes"
+// import { Link } from "react-router-dom";
+import React, { createContext, useState } from 'react'
+import {  ConfigProvider, theme, Input } from 'antd';
+import Layout from "./layout";
+import { MoralisProvider } from "react-moralis"
+export const UserContext = createContext()
+const App:React.FC = () =>{
+    return (
+        <MoralisProvider initializeOnMount={false}>
+        <div className="App">
+            <ConfigProvider
+                theme={{
+                    token:{
+                        colorPrimary: '#FF7676',
+                        colorText: '#ffffff',
+                        colorIconHover: 'rgba(255, 255, 255, 0.88)',
+                        colorIcon: 'rgba(255, 255, 255, 0.45)',
+                        colorBgContainer: 'rgba(0, 0, 0, 0.88)',
+                        colorBgElevated: 'rgba(0, 0, 0, 0.88)'
+                    }
+                }}
+            >
+                <Layout />
+            </ConfigProvider>
+
+        </div>
+        </MoralisProvider>
+    )
+}
+
+
+export default App
